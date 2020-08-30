70c4938a138e389ef251008407ab6c699f309518
engrkartik <you@example.com>
2020-08-30 15:10:30 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      body: (Text('Hello I am Here')),
+      appBar: AppBar(
+        title: Text('Doctors Appointment'),
+        backgroundColor: Colors.yellow[900],
+      ),
+    ),
+
+  ));
+}
